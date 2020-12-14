f473ee602a308fae19206010fef2d30370ac248b
喵喵拳 <miaomiaoquanljj@163.com>
2020-12-14 11:29:10 +0800

    第一次提交，初始化项目

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastclick from 'fastclick'
+import './assets/css/base.scss'
 
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.(html)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    devServer: {
+        open: true,
+    }
+}
